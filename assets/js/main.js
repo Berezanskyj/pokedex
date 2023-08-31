f787e276ce0830b07b96fcc3b806997503734047
Rafael <berezanskyj16@gmail.com>
2023-08-30 23:41:29 -0300

    convertendo a response em json.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,8 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
 
-// Pegando uma resposta da API
-fetch(url).then(function (reponse){
-    console.log(reponse)
-})
+
+fetch(url)
+    .then((reponse) => reponse.json())  // Metodo then -- Pega a resposta da requisicao
+    .then((jsonBody) => console.log(jsonBody))  // Recebendo o body convertido em JSON  printando ele 
+    .catch((error) => console.error(error))  // Metodo catch -- Pega o erro da requisica
