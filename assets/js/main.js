4d9d70fc7d7f9fbc9aacb321698a61804912cc3d
Rafael <berezanskyj16@gmail.com>
2023-08-30 23:02:54 -0300

    Adicionando recursos adicionais e comecando a utilizar a API.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('Deu certo')
+// Declarando Variaveis
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+
+// Pegando uma resposta da API
+fetch(url).then(function (reponse){
+    console.log(reponse)
+})
